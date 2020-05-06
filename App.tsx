890a8d842743439810e0657e2692738bc638db49
Brent Vatne <brentvatne@gmail.com>
2020-05-06 16:39:29 -0700

    Example web font issue

--- App.tsx
+++ App.tsx
@@ -1,19 +1,76 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+
+function FirstFont() {
+  // alert('FirstFont component rendered!');
+  let isReady = useFont({ ExampleOne: require("./assets/example.ttf") });
+
+  if (!isReady) {
+    return <Text />;
+  } else {
+    return (
+      <Text style={{ fontFamily: "ExampleOne", fontSize: 40, color: "green" }}>
+        Example one!
+      </Text>
+    );
+  }
+}
+
+function SecondFont() {
+  let isReady = useFont({ ExampleTwo: require("./assets/example2.ttf") });
+
+  if (!isReady) {
+    return <Text />;
+  } else {
+    return (
+      <Text style={{ fontFamily: "ExampleTwo", fontSize: 40, color: "red" }}>
+        Example two
+      </Text>
+    );
+  }
+}
+
+function MaybeSecondFont() {
+  const [showSecondFont, setShowSecondFont] = React.useState(false);
+
+  return showSecondFont ? (
+    <SecondFont />
+  ) : (
+    <Button title="Show second font" onPress={() => setShowSecondFont(true)} />
+  );
+}
 
 export default function App() {
+  // alert('App component rendered!');
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <FirstFont />
+      <MaybeSecondFont />
     </View>
   );
+}
+
+function useFont(font: any) {
+  const [isReady, setIsReady] = React.useState(false);
+
+  React.useEffect(() => {
+    async function loadMyFontAsync() {
+      await Font.loadAsync(font);
+      setIsReady(true);
+    }
+
+    loadMyFontAsync();
+  }, []);
+
+  return isReady;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 30,
+    backgroundColor: "#fff",
+    alignItems: "center",
   },
 });
